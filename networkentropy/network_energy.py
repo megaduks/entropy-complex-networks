5670a6f6bc08eb4a6c81cab0c4c6142bc81cdf58
Mikołaj Morzy <mikolaj.morzy@gmail.com>
2018-01-30 01:50:27 +0100

    Added Laplacian energy and graph energy

--- networkentropy/network_energy.py
+++ networkentropy/network_energy.py
@@ -3,6 +3,7 @@ import networkx as nx
 import scipy
 from itertools import product
 
+
 def get_randic_matrix(G):
     """
     Computes the Randić matrix of a graph
@@ -19,10 +20,11 @@ def get_randic_matrix(G):
 
     D = nx.degree(G)
 
-    randic_values = [ 1/np.sqrt(D[v]*D[w]) if G.has_edge(v,w) else 0 for (v,w) in product(G.nodes, G.nodes) ]
+    randic_values = [1 / np.sqrt(D[v] * D[w]) if G.has_edge(v, w) else 0 for (v, w) in product(G.nodes, G.nodes)]
     randic_matrix = np.matrix(randic_values).reshape(G.number_of_nodes(), G.number_of_nodes())
 
     return randic_matrix
+
 
 def get_randic_index(G):
     """
@@ -41,6 +43,7 @@ def get_randic_index(G):
 
     return randic_index
 
+
 def get_randic_energy(G):
     """
     Computes the Randić energy of a graph
@@ -51,7 +54,39 @@ def get_randic_energy(G):
     :return: float: Randić energy of a graph
     """
 
-    rm = get_randic_matrix(G)
-    randic_energy = np.abs(scipy.linalg.eigvals(rm).real).sum()
+    R = get_randic_matrix(G)
+    randic_energy = np.abs(scipy.linalg.eigvals(R).real).sum()
 
     return randic_energy
+
+
+def get_laplacian_energy(G):
+    """
+    Computes the energy of the Laplacian of a graph
+
+    :param G: input graph
+    :return: float: Laplacian energy of a graph
+    """
+
+    L = nx.laplacian_matrix(G)
+    laplacian_energy = np.abs(scipy.linalg.eigvals(L).real).sum()
+
+    return laplacian_energy
+
+
+def get_graph_energy(G):
+    """
+    Computes the energy of the adjacency matrix of a graph
+
+    :param G: input graph
+    :return: float: energy of the adjacency matrix of a graph
+    """
+
+    M = nx.adjacency_matrix(G).todense()
+    graph_energy = np.abs(scipy.linalg.eigvals(M).real).sum()
+
+    return graph_energy
+
+# TODO compute the disntace matrix for a graph
+# TODO compute the distance energy for a graph
+# TODO add the retrieval of spectra from a graph
