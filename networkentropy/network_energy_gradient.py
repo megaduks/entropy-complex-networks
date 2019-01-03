7e8ecdf2f4ee7dc2a403c6f9d5c2cda46ccdac28
tomi <tomi.git@gmail.com>
2019-01-03 23:49:31 +0100

    Delete unnecessary methods

--- networkentropy/network_energy_gradient.py
+++ networkentropy/network_energy_gradient.py
@@ -4,7 +4,7 @@ import networkx as nx
 from networkentropy import network_energy as ne
 
 
-def get_energy_method(method: str) -> Callable[[nx.Graph], float]:
+def get_energy_method(method: str) -> Callable[[nx.Graph, int], Dict]:
     """
     Returns one of methods for computing graph energy
 
@@ -12,11 +12,11 @@ def get_energy_method(method: str) -> Callable[[nx.Graph], float]:
     :return: method for computing graph energy
     """
     if method == "randic":
-        return ne.get_randic_energy
+        return ne.randic_centrality
     elif method == "laplacian":
-        return ne.get_laplacian_energy
+        return ne.laplacian_centrality
     elif method == "graph":
-        return ne.get_graph_energy
+        return ne.graph_energy_centrality
     else:
         raise ValueError("Method: {} doesn't exist".format(method))
 
@@ -25,30 +25,14 @@ def __compute_gradient(energy1: float, energy2: float) -> float:
     return energy2 - energy1
 
 
-def get_energy_gradient(G1: nx.Graph, G2: nx.Graph, method: str) -> float:
-    """
-    Computes energy gradient between two graphs using a given method. Energy of G2 graph is subtracted from
-    energy of G1 graph.
-
-    :param G1: first graph
-    :param G2: second graph
-    :param method: name of a method for computing graph energy. Possible values are: randic, laplacian, graph
-    :return: energy gradient between the two graphs
-    """
-    get_energy = get_energy_method(method)
-    return __compute_gradient(get_energy(G1), get_energy(G2))
-
-
-def get_energy_gradient_of_edge(G: nx.Graph, edge: Tuple, method: str, radius: int = 1):
-    G1 = nx.ego_graph(G, edge[0], radius=radius)
-    G2 = nx.ego_graph(G, edge[1], radius=radius)
-    return get_energy_gradient(G1, G2, method)
-
-
 def get_energy_gradients(G: nx.Graph, method: str, complete: bool = True, radius: int = 1) -> Dict[Tuple, float]:
+    get_energies = get_energy_method(method)
+    energies = get_energies(G, radius)
     result = {}
     for edge in G.edges:
-        gradient = get_energy_gradient_of_edge(G, edge, method, radius)
+        node1 = edge[0]
+        node2 = edge[1]
+        gradient = __compute_gradient(energies[node1], energies[node2])
         result[edge] = gradient
         if complete:
             result[edge[::-1]] = -gradient
@@ -56,19 +40,18 @@ def get_energy_gradients(G: nx.Graph, method: str, complete: bool = True, radius
 
 
 def get_graph_with_energy_data(G: nx.Graph, methods: Tuple, radius: int = 1, copy: bool = True):
-    energy_functions = {}
+    energy_methods = {}
     for m in methods:
-        energy_functions[m] = get_energy_method(m)
+        energy_methods[m] = get_energy_method(m)
     if copy:
         G = G.copy()
-    for edge in G.edges:
-        node1 = edge[0]
-        node2 = edge[1]
-        G1 = nx.ego_graph(G, node1, radius=radius)
-        G2 = nx.ego_graph(G, node2, radius=radius)
-        for method, get_energy in energy_functions.items():
-            energyG1 = get_energy(G1)
-            energyG2 = get_energy(G2)
+    for method, get_energy in energy_methods.items():
+        energies = get_energy(G, radius)
+        for edge in G.edges:
+            node1 = edge[0]
+            node2 = edge[1]
+            energyG1 = energies[node1]
+            energyG2 = energies[node2]
             G.node[node1]["{}_energy".format(method)] = energyG1
             G.node[node2]["{}_energy".format(method)] = energyG2
             G[node1][node2]["{}_gradient".format(method)] = __compute_gradient(energyG1, energyG2)
--- networkentropy/tests/test_network_energy_gradient.py
+++ networkentropy/tests/test_network_energy_gradient.py
@@ -17,20 +17,10 @@ class NetworkEnergyGradientTest(TestCase):
         ]).reshape((4, 4))
         self.G = nx.from_numpy_array(graph_matrix)
 
-    def test_get_energy_gradient(self):
-        G1 = nx.ego_graph(self.G, 0, radius=1)
-        G2 = nx.ego_graph(self.G, 1, radius=1)
-        actual_graph_gradient = neg.get_energy_gradient(G1, G2, method="graph")
-        actual_laplacian_gradient = neg.get_energy_gradient(G1, G2, method="laplacian")
-        actual_randic_gradient = neg.get_energy_gradient(G1, G2, method="randic")
-        assert actual_graph_gradient == -2.9623886081840283
-        assert actual_laplacian_gradient == -4.0
-        assert actual_randic_gradient == -0.45742710775633855
-
     def test_get_energy_gradients(self):
-        actual_graph_gradients = neg.get_energy_gradients(self.G, "graph")
-        actual_laplacian_gradients = neg.get_energy_gradients(self.G, "laplacian")
-        actual_randic_gradients = neg.get_energy_gradients(self.G, "randic")
+        actual_graph_gradients = neg.get_energy_gradients(self.G, "graph", complete=False)
+        actual_laplacian_gradients = neg.get_energy_gradients(self.G, "laplacian", complete=False)
+        actual_randic_gradients = neg.get_energy_gradients(self.G, "randic", complete=False)
         expected_graph_gradients = {(0, 1): -2.9623886081840283, (0, 2): -0.9623886081840287,
                                     (0, 3): -0.9623886081840287, (2, 3): 0.0}
         expected_laplacian_gradients = {(0, 1): -4.0, (0, 2): -2.000000000000001,
