9c7d5f7f7766f10a580ce49db6d849ee46023004
tomi <tomi.git@gmail.com>
2019-04-22 20:26:21 +0200

    Add method for calculating sum of gradients for a path

--- networkentropy/tests/test_network_energy_gradient.py
+++ networkentropy/tests/test_network_energy_gradient.py
@@ -4,9 +4,15 @@ import numpy as np
 
 import networkentropy.network_energy_gradient as neg
 
+RANDIC = 'randic'
+
+LAPLACIAN = 'laplacian'
+
+GRAPH = 'graph'
+
 
 class NetworkEnergyGradientTest(TestCase):
-    G = None
+    g = None
 
     def setUp(self):
         graph_matrix = np.array([
@@ -15,12 +21,12 @@ class NetworkEnergyGradientTest(TestCase):
             0, 0, 0, 1,
             0, 0, 0, 0
         ]).reshape((4, 4))
-        self.G = nx.from_numpy_array(graph_matrix)
+        self.g = nx.from_numpy_array(graph_matrix)
 
     def test_get_energy_gradients(self):
-        actual_graph_gradients = neg.get_energy_gradients(self.G, "graph", complete=False)
-        actual_laplacian_gradients = neg.get_energy_gradients(self.G, "laplacian", complete=False)
-        actual_randic_gradients = neg.get_energy_gradients(self.G, "randic", complete=False)
+        actual_graph_gradients = neg.get_energy_gradients(self.g, GRAPH, complete=False)
+        actual_laplacian_gradients = neg.get_energy_gradients(self.g, LAPLACIAN, complete=False)
+        actual_randic_gradients = neg.get_energy_gradients(self.g, RANDIC, complete=False)
         expected_graph_gradients = {(0, 1): -2.9623886081840283, (0, 2): -0.9623886081840287,
                                     (0, 3): -0.9623886081840287, (2, 3): 0.0}
         expected_laplacian_gradients = {(0, 1): -4.0, (0, 2): -2.000000000000001,
@@ -30,3 +36,22 @@ class NetworkEnergyGradientTest(TestCase):
         assert actual_graph_gradients == expected_graph_gradients
         assert actual_laplacian_gradients == expected_laplacian_gradients
         assert actual_randic_gradients == expected_randic_gradients
+
+    def test_graph_decorator_path_gradient(self):
+        for node in self.g.nodes():
+            self.g.node[node][neg._get_energy_method_name(GRAPH)] = node
+            self.g.node[node][neg._get_energy_method_name(LAPLACIAN)] = 2 * node
+            self.g.node[node][neg._get_energy_method_name(RANDIC)] = 3 * node
+
+        decorated_graph = neg._decorate_graph(self.g, (GRAPH, LAPLACIAN, RANDIC))
+        path = [2, 0, 1]
+
+        path_graph_gradient = decorated_graph.get_path_gradient(path, GRAPH)
+        path_laplacian_gradient = decorated_graph.get_path_gradient(path, LAPLACIAN)
+        path_randic_gradient = decorated_graph.get_path_gradient(path, RANDIC)
+
+        assert path_graph_gradient == -1
+        assert path_laplacian_gradient == -2
+        assert path_randic_gradient == -3
+
+
