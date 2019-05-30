9f0ce4435fcdbb319852052a99630f41de6c966e
Mikołaj Morzy <mikolaj.morzy@gmail.com>
2019-05-30 10:28:06 +0200

    Set up initial experiment with three generative models

--- networkentropy/sampling.py
+++ networkentropy/sampling.py
@@ -1,7 +1,9 @@
 from typing import Dict
+from tqdm import tqdm
 
 import networkx as nx
 import numpy as np
+import pandas as pd
 
 from node2vec import Node2Vec
 from scipy.stats import ks_2samp
@@ -65,7 +67,7 @@ def random_degree(graph: nx.Graph, sample_ratio: float) -> nx.Graph:
 
     num_nodes = int(sample_ratio * nx.number_of_nodes(graph))
     degree_sum = sum(dict(graph.degree).values())
-    degree_probs = [d/degree_sum for n,d in graph.degree]
+    degree_probs = [d / degree_sum for n, d in graph.degree]
     sample_nodes = np.random.choice(graph.nodes, size=num_nodes, replace=False, p=degree_probs)
 
     return nx.subgraph(graph, sample_nodes)
@@ -85,7 +87,7 @@ def random_pagerank(graph: nx.Graph, sample_ratio: float) -> nx.Graph:
 
     num_nodes = int(sample_ratio * nx.number_of_nodes(graph))
     pagerank_sum = sum(dict(nx.pagerank(graph)).values())
-    pagerank_probs = [p/pagerank_sum for p in dict(nx.pagerank(graph)).values()]
+    pagerank_probs = [p / pagerank_sum for p in dict(nx.pagerank(graph)).values()]
     sample_nodes = np.random.choice(graph.nodes, size=num_nodes, replace=False, p=pagerank_probs)
 
     return nx.subgraph(graph, sample_nodes)
@@ -105,7 +107,7 @@ def random_energy_gradient(graph: nx.Graph, sample_ratio: float) -> nx.Graph:
 
     num_nodes = int(sample_ratio * nx.number_of_nodes(graph))
     energy_gradient_sum = sum(dict(ne.graph_energy_gradient_centrality(graph)).values())
-    energy_gradient_probs = [p/energy_gradient_sum for p in dict(ne.graph_energy_gradient_centrality(graph)).values()]
+    energy_gradient_probs = [p / energy_gradient_sum for p in dict(ne.graph_energy_gradient_centrality(graph)).values()]
     sample_nodes = np.random.choice(graph.nodes, size=num_nodes, replace=False, p=energy_gradient_probs)
 
     return nx.subgraph(graph, sample_nodes)
@@ -136,7 +138,7 @@ def random_embedding(graph: nx.Graph, sample_ratio: float) -> nx.Graph:
 
         for dim in range(embedded_graph.wv.vectors.shape[-1]):
             col_values = np.squeeze(np.asarray(embedded_graph.wv.vectors[:, dim]))
-            col_probs = np.asarray(probabilities[:,dim])
+            col_probs = np.asarray(probabilities[:, dim])
             # normalize probabilities to make sure they sum up to 1.0
             col_probs /= col_probs.sum()
             random_vector = np.append(random_vector, np.random.choice(col_values, size=1, p=col_probs))
@@ -149,7 +151,7 @@ def random_embedding(graph: nx.Graph, sample_ratio: float) -> nx.Graph:
     return nx.subgraph(graph, sample_nodes)
 
 
-def compare_graphs(g1: nx.Graph, g2: nx.Graph) -> Dict :
+def compare_graphs(g1: nx.Graph, g2: nx.Graph) -> Dict:
     """
     Compares the original graph with the sampled graph in terms of basic graph descriptors
     by performing KS-test on normalized distributions
@@ -158,11 +160,11 @@ def compare_graphs(g1: nx.Graph, g2: nx.Graph) -> Dict :
     :param g2: sampled graph
     :return: dictionary with the results of multiple comparisons
     """
-    #TODO: allow to specify which features are to be used in comparison
+    # TODO: allow to specify which features are to be used in comparison
 
     results = {}
 
-    for k,f in {
+    for k, f in {
         'degree': nx.degree_centrality,
         'betweenness': nx.betweenness_centrality,
         'pagerank': nx.pagerank,
@@ -180,11 +182,11 @@ def compare_graphs(g1: nx.Graph, g2: nx.Graph) -> Dict :
 
         # normalize distributions
         if sum(x) > 0:
-            x = [e/sum(x) for e in x]
+            x = [e / sum(x) for e in x]
         if sum(y) > 0:
-            y = [e/sum(y) for e in y]
+            y = [e / sum(y) for e in y]
 
-        stat, p_val = ks_2samp(x,y)
+        stat, p_val = ks_2samp(x, y)
 
         results.update({k: {'stat': stat, 'p_val': p_val}})
 
@@ -193,15 +195,48 @@ def compare_graphs(g1: nx.Graph, g2: nx.Graph) -> Dict :
 
 if __name__ == '__main__':
 
-    g = nx.erdos_renyi_graph(100,0.1)
+    results = list()
 
-    for i in range(1,100):
-        gg = random_energy_gradient(g, sample_ratio=i/100)
-        result = compare_graphs(g, gg)
+    num_nodes = 100
 
-        print(f"{i}: degree: {result['degree']['p_val']:.4f} "
-              f"betweenness: {result['betweenness']['p_val']:.4f} "
-              f"pagerank: {result['pagerank']['p_val']:.4f} "
-              f"closeness: {result['closeness']['p_val']:.4f} "
-              f"clustering: {result['clustering']['p_val']:.4f} "
-              )
+    # iterate over graph model main parameter
+    for i in tqdm(range(1, 100)):
+
+        graph_models = {
+            'random': (nx.erdos_renyi_graph(n=num_nodes, p=i)),
+            'smallworld': nx.watts_strogatz_graph(n=num_nodes, k=4, p=i / 10),
+            'powerlaw': nx.barabasi_albert_graph(n=num_nodes, m=2)
+        }
+
+        # iterate over graph models
+        for graph in graph_models:
+
+            g = graph_models[graph]
+
+            for j in tqdm(range(1, 100)):
+                sg = random_energy_gradient(g, sample_ratio=j/100)
+
+                result = compare_graphs(g, sg)
+
+                results.append(
+                    (
+                        graph,
+                        i,
+                        j,
+                        result['degree']['p_val'],
+                        result['betweenness']['p_val'],
+                        result['pagerank']['p_val'],
+                        result['closeness']['p_val'],
+                        result['clustering']['p_val']
+                    )
+                )
+
+    pd.DataFrame(results,
+                 columns=['model',
+                          'param',
+                          'sample_ratio',
+                          'degree',
+                          'betweenness',
+                          'pagerank',
+                          'closeness',
+                          'clustering']).to_csv('results.csv')
