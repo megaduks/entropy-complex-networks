a9d3337385d44aea7cddf0b3db6a730609b86086
Mikołaj Morzy <mikolaj.morzy@gmail.com>
2019-06-13 00:28:20 +0200

    Added graph energy sampling

--- networkentropy/sampling.py
+++ networkentropy/sampling.py
@@ -114,6 +114,27 @@ def random_energy_gradient(graph: nx.Graph, sample_ratio: float) -> nx.Graph:
     return nx.subgraph(graph, sample_nodes)
 
 
+def random_graph_energy(graph: nx.Graph, sample_ratio: float) -> nx.Graph:
+    """
+    Samples a graph by drawing a sample of nodes with the probability of drawing a node being
+    proportional to node's centrality computed from graph energy (energy of node's neighborhood)
+
+    :param graph: input graph
+    :param sample_ratio: percentage of the original nodes to be sampled
+    :return: a random subgraph
+    """
+
+    assert 0 <= sample_ratio <= 1, 'sample_ratio must be between [0, 1]'
+
+    num_nodes = int(sample_ratio * nx.number_of_nodes(graph))
+    graph_energy_sum = sum(dict(ne.graph_energy_centrality(graph)).values())
+    graph_energy_probs = [p / graph_energy_sum for p in dict(ne.graph_energy_centrality(graph)).values()]
+    sample_nodes = np.random.choice(graph.nodes, size=num_nodes, replace=False, p=graph_energy_probs)
+
+    return nx.subgraph(graph, sample_nodes)
+
+
+
 def random_embedding(graph: nx.Graph, sample_ratio: float) -> nx.Graph:
     """
     Samples a graph by drawing random vectors from the embedding matrix
