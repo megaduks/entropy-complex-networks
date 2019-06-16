7be0d7c2b2ba61b3b9b1555ab63a33375a27fb64
Mikołaj Morzy <mikolaj.morzy@gmail.com>
2019-06-16 19:32:25 +0200

    Added node embeddings based on energy gradients

--- networkentropy/tests/test_embed.py
+++ networkentropy/tests/test_embed.py
@@ -29,7 +29,35 @@ class EmbedTests(unittest.TestCase):
         self.assertEqual(vec_number, len(self.G.nodes))
         self.assertEqual(vec_size, embedding_size)
 
+    def test_embedding_with_gradient_walk(self):
+        embedding_size = 200
+        node_embeddings = embed.node2vec(self.G, embedding_size=embedding_size, walk_type='gradient')
+
+        vec_number, vec_size = node_embeddings.wv.vectors.shape
+
+        self.assertEqual(vec_number, len(self.G.nodes))
+        self.assertEqual(vec_size, embedding_size)
+
+    def test_embedding_with_gradient_walk_laplacian_energy(self):
+        embedding_size = 200
+        node_embeddings = embed.node2vec(self.G, embedding_size=embedding_size, walk_type='gradient',
+                                         energy_type='laplacian')
+
+        vec_number, vec_size = node_embeddings.wv.vectors.shape
+
+        self.assertEqual(vec_number, len(self.G.nodes))
+        self.assertEqual(vec_size, embedding_size)
+
+    def test_embedding_with_gradient_walk_randic_energy(self):
+        embedding_size = 200
+        node_embeddings = embed.node2vec(self.G, embedding_size=embedding_size, walk_type='gradient',
+                                         energy_type='randic')
+
+        vec_number, vec_size = node_embeddings.wv.vectors.shape
+
+        self.assertEqual(vec_number, len(self.G.nodes))
+        self.assertEqual(vec_size, embedding_size)
+
 
 if __name__ == '__main__':
-
     unittest.main()
